02b86b7cad7b8e79864420785f1cb28824924c9a
Hendrich <hendrich@tams150.informatik.uni-hamburg.de>
2015-12-24 11:01:19 +0100

    Replace backquote "`" with normal quote "'" to fix syntax-error in firefox.

--- gates.js
+++ gates.js
@@ -11,7 +11,7 @@
 
   var exclusion = function(target) {
     return appendToSVG(target,'path',{
-      d: `M-50,35c0,0 17.5,-3 17.5,-35c0,-32.5 -17.5,-35 -17.5,-35`,
+      d: 'M-50,35c0,0 17.5,-3 17.5,-35c0,-32.5 -17.5,-35 -17.5,-35',
       class: 'gate-body-extra'
     });
   };
@@ -28,21 +28,21 @@
 
   var or = function(target) {
     return appendToSVG(target,'path',{
-      d: `M-40,35c-0,0 17.5,-3 17.5,-35c0,-32.5 -17.5,-35 -17.5,-35c27.5,3 45,0 65,35c-15,32 -32.5,32.5 -65,35Z`,
+      d: 'M-40,35c-0,0 17.5,-3 17.5,-35c0,-32.5 -17.5,-35 -17.5,-35c27.5,3 45,0 65,35c-15,32 -32.5,32.5 -65,35Z',
       class: 'gate-body'
     });
   };
 
   var and = function(target) {
     return appendToSVG(target,'path',{
-      d: `M-5,35l-30,0l0,-70l30.194,0c16.366,0 29.806,15.68 29.806,35c0,19.32 -13.44,35 -30,35Z`,
+      d: 'M-5,35l-30,0l0,-70l30.194,0c16.366,0 29.806,15.68 29.806,35c0,19.32 -13.44,35 -30,35Z',
       class: 'gate-body'
     });
   };
 
   var buffer = function(target) {
     return appendToSVG(target,'path',{
-      d: `M-25,-35l50,35l-50,35l0,-70Z`,
+      d: 'M-25,-35l50,35l-50,35l0,-70Z',
       class: 'gate-body'
     });
   };
